a8b7d3763776558a5949875b45008b48d2fa7f0b
jafore <princejafore@gmail.com>
2025-05-05 23:14:23 +0600

    Dom Manupulation

--- dom.js
+++ dom.js
@@ -1,21 +1,21 @@
-// let person={
-//     name:"rashed",
-//     mobile:"012235566",
-//     address:{
-//         village:"Horogram",
-//         road:'01',
-//         post:"Rajshahi Court",
-//         geolocation:{
-//              latitude:12220001,
-//              logitude:5566988
-//         }
-//     }
-// }
+let person={
+    name:"rashed",
+    mobile:"012235566",
+    address:{
+        village:"Horogram",
+        road:'01',
+        post:"Rajshahi Court",
+        geolocation:{
+             latitude:12220001,
+             logitude:5566988
+        }
+    }
+}
 
 // console.log(person);
 
 // console.log(window);
 console.log(document)
-console.log(document.get)
+// console.log(document.get)
 
 
